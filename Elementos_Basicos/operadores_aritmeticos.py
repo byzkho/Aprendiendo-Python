66862a77f8028280aa7eb2bd1483543034cbebb3
byzkho <ruddyskrrasco@gmail.com>
2024-03-26 02:05:34 -0400

    mi primer commit

---
+++ Elementos_Basicos/operadores_aritmeticos.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 5
+
+resultado = num1 + num2
+print(num1 + num2)
+
+'''Prioridad de los operadores aritmeticos:
+   1ro. Parentesis ()
+   2do. Exponenciacion **
+   3ro. Multiplicacion, Division y Modulo * / %
+   4to. Suma y Resta + -
+'''
+resultado = 3**3*(13/5-(2*4))
+print(resultado)
---
+++ Elementos_Basicos/tipos_datos.py
@@ -0,0 +1,30 @@
+
+# Tipos de datos
+numero = 10
+
+print(numero)
+print(type(numero))
+
+cadena = 'Ruddys "es mi nombre".'
+print(cadena)
+print(type(cadena))
+
+valor = True
+print(valor)
+print(type(valor))
+
+num1 = 10
+num2 = 2.5
+resultado = (num1 + num2) * 5 / 3
+print("El resultado es:", resultado)
+
+# Tipado dinamico
+valor = 10
+print(valor)
+valor = "Ruddys"
+print(valor)
+
+'''
+Este es un comentario 
+multilineado en el texto
+'''
